62d9d7cf48c62bf20ef62753fff2d871964266ba
yskim6772 <yskim6772@gmail.com>
2024-04-25 02:48:15 +0900

    Refactor: 추상 클래스 BindingFragment 생성

---
+++ app/src/main/java/com/sopt/now/BindingFragment.kt
@@ -0,0 +1,30 @@
+package com.sopt.now
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BindingFragment<T : ViewBinding>(
+    private val inflate: (LayoutInflater, ViewGroup?, Boolean) -> T
+) : Fragment() {
+    private var _binding: T? = null
+    protected val binding: T
+        get() = _binding ?: throw IllegalStateException("ViewBinding is not initialized")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflate.invoke(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
