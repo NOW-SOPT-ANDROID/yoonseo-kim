5193d3c30b8e85563f149d9331b2f2d86eec5287
yskim6772 <yskim6772@gmail.com>
2024-04-25 13:40:37 +0900

    Refactor: with(binding) 사용

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -1,38 +1,32 @@
 package com.sopt.now
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.sopt.now.UserInfo
 import com.sopt.now.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    companion object {
-        const val USER_INFO = "user_info"
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // LoginActivity로부터 전달된 사용자 정보를 받음
         val userInfo = intent?.getParcelableExtra<UserInfo>(USER_INFO)
 
-        // 사용자 정보가 null이 아니라면 TextView에 정보 표시
         userInfo?.let {
-            binding.tvMyPageNickname.text = it.nickname
-            binding.tvMyPageMbti.text = it.mbti
-            binding.tvMyPageId.text = it.id
-            binding.tvMyPagePwd.text = it.password
+            with(binding) {
+                tvMyPageNickname.text = it.nickname
+                tvMyPageMbti.text = it.mbti
+                tvMyPageId.text = it.id
+                tvMyPagePwd.text = it.password
+            }
         }
+    }
+
+    companion object {
+        const val USER_INFO = "user_info"
     }
 }
