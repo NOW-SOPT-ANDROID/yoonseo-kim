19801c95606f7546aeffd92a6af2d07166a562b7
yskim6772 <yskim6772@gmail.com>
2024-05-22 14:53:43 +0900

    Refactor: 폴더링 변경

--- app/src/main/java/com/sopt/now/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/sopt/now/presentation/search/SearchFragment.kt
@@ -2,7 +2,7 @@ package com.sopt.now.presentation.search
 
 import android.os.Bundle
 import android.view.View
-import com.sopt.now.BindingFragment
+import com.sopt.now.util.base.BindingFragment
 import com.sopt.now.databinding.FragmentSearchBinding
 
 class SearchFragment : BindingFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate) {
