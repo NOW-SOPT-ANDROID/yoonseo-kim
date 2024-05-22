27c18089deea113c7276b325d5fafc72a640f2d1
yskim6772 <yskim6772@gmail.com>
2024-05-22 14:52:05 +0900

    Feat: AuthService 코루틴 적용

--- app/src/main/java/com/sopt/now/data/service/AuthService.kt
+++ app/src/main/java/com/sopt/now/data/service/AuthService.kt
@@ -5,18 +5,18 @@ import com.sopt.now.data.dto.request.RequestSignUpDto
 import com.sopt.now.data.dto.response.ResponseLoginDto
 import com.sopt.now.data.dto.response.ResponseSignUpDto
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
-    // 코루틴 서버 통신 - DTO 자체를 리턴 타입으로
     @POST("member/join")
     suspend fun signUp(
         @Body request: RequestSignUpDto,
     ): ResponseSignUpDto
 
     @POST("member/login")
-    fun login(
+    suspend fun login(
         @Body request: RequestLoginDto,
-    ): Call<ResponseLoginDto>
+    ): Response<ResponseLoginDto>
 }
