8cdb357af62c743b47b66086f0f47e6823a04462
yskim6772 <yskim6772@gmail.com>
2024-05-23 15:45:24 +0900

    Add: MainViewModel 추가

---
+++ app/src/main/java/com/sopt/now/compose/presentation/main/MainViewModel.kt
@@ -0,0 +1,59 @@
+package com.sopt.now.compose.presentation.main
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sopt.now.compose.data.ServicePool
+import com.sopt.now.compose.data.ServicePool.userService
+import com.sopt.now.compose.data.dto.response.ResponseUserInfoDto
+import com.sopt.now.compose.presentation.signup.SignUpState
+import com.sopt.now.compose.presentation.user.UserInfo
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel : ViewModel() {
+
+    private val userService by lazy { ServicePool.userService }
+
+    private val _userInfo = MutableLiveData<UserInfo?>()
+    val userInfo: LiveData<UserInfo?> get() = _userInfo
+
+    init {
+        getUserInfo(USER_ID)
+    }
+
+    fun getUserInfo(userId: Int) {
+        viewModelScope.launch {
+            userService.getUserInfo(userId).enqueue(object : Callback<ResponseUserInfoDto> {
+                override fun onResponse(
+                    call: Call<ResponseUserInfoDto>,
+                    response: Response<ResponseUserInfoDto>,
+                ) {
+                    if (response.isSuccessful) {
+                        response.body()?.data?.let { userInfoDto ->
+                            _userInfo.value = UserInfo(
+                                nickname = userInfoDto.nickname,
+                                phone = userInfoDto.phone,
+                                authenticationId = userInfoDto.authenticationId
+                            )
+                        }
+                    } else {
+                        Log.d("MyPage", "Error: ${response.errorBody()?.string() ?: response.message()}")
+                    }
+                }
+
+                override fun onFailure(call: Call<ResponseUserInfoDto>, t: Throwable) {
+                    Log.d("MyPage", "onFailure", t)
+                }
+            })
+        }
+    }
+
+    companion object {
+        const val USER_ID = 51
+    }
+}
