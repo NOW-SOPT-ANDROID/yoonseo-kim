d321ceaa90f019f7a756f71cf99b375b5c0bf80b
yskim6772 <yskim6772@gmail.com>
2024-06-03 19:43:01 +0900

    Del: UserInfo parcelable 적용 제거

--- app/src/main/java/com/sopt/now/compose/presentation/user/UserInfo.kt
+++ app/src/main/java/com/sopt/now/compose/presentation/user/UserInfo.kt
@@ -1,11 +1,10 @@
 package com.sopt.now.compose.presentation.user
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
 
-@Parcelize
+@Serializable
 data class UserInfo(
     val authenticationId: String,
     val nickname: String,
     val phone: String
-) : Parcelable
+)
