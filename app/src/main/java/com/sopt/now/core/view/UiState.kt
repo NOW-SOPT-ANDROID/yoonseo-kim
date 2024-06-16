32f2944355c4a6d85d85e9332a204b6f49ec2efc
yskim6772 <yskim6772@gmail.com>
2024-06-17 03:55:35 +0900

    Chore: 초기화 적용

--- app/src/main/java/com/sopt/now/core/view/UiState.kt
+++ app/src/main/java/com/sopt/now/core/view/UiState.kt
@@ -1,6 +1,6 @@
 package com.sopt.now.core.view
 
 data class UiState(
-    val isSuccess: Boolean,
-    val message: String
+    val isSuccess: Boolean = false,
+    val message: String = ""
 )
--- app/src/main/java/com/sopt/now/presentation/user/UserInfo.kt
+++ app/src/main/java/com/sopt/now/presentation/user/UserInfo.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class UserInfo(
-    val authenticationId: String,
-    val nickname: String,
-    val phone: String
+    val authenticationId: String = "",
+    val nickname: String = "",
+    val phone: String = ""
 )
