f455299499b95d39b59ca38a2e434ff8ad1aaa17
yskim6772 <yskim6772@gmail.com>
2024-06-27 13:00:32 +0900

    Refactor: 의존성 수동 주입 제거 및 코드 수정

--- app/src/main/java/com/sopt/now/core/base/factory/BaseViewModelFactory.kt
+++ app/src/main/java/com/sopt/now/core/base/factory/BaseViewModelFactory.kt
@@ -1,7 +1,19 @@
 package com.sopt.now.core.base.factory
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.sopt.now.data.ApiFactory
+import com.sopt.now.data.ServicePool
+import com.sopt.now.data.datasource.AuthDataSource
+import com.sopt.now.data.datasource.FriendDataSource
+import com.sopt.now.data.datasource.UserDataSource
+import com.sopt.now.data.datasourceImpl.AuthDataSourceImpl
+import com.sopt.now.data.datasourceImpl.FriendDataSourceImpl
+import com.sopt.now.data.datasourceImpl.UserDataSourceImpl
+import com.sopt.now.data.repoImpl.AuthRepositoryImpl
+import com.sopt.now.data.repoImpl.FriendRepositoryImpl
+import com.sopt.now.data.repoImpl.UserRepositoryImpl
 import com.sopt.now.domain.repository.AuthRepository
 import com.sopt.now.domain.repository.FriendRepository
 import com.sopt.now.domain.repository.UserRepository
@@ -11,28 +23,28 @@ import com.sopt.now.presentation.main.MainViewModel
 import com.sopt.now.presentation.signup.SignUpViewModel
 
 class BaseViewModelFactory(
-    private val authRepository: AuthRepository? = null,
-    private val friendRepository: FriendRepository? = null,
-    private val userRepository: UserRepository? = null
+    private val context: Context
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return authRepository?.let { LoginViewModel(it) } as T
+        return when {
+            modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
+                val repository = AuthRepositoryImpl(AuthDataSourceImpl(ServicePool.authService))
+                LoginViewModel(repository) as T
+            }
+            modelClass.isAssignableFrom(SignUpViewModel::class.java) -> {
+                val repository = AuthRepositoryImpl(AuthDataSourceImpl(ServicePool.authService))
+                SignUpViewModel(repository) as T
+            }
+            modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
+                val repository = FriendRepositoryImpl(FriendDataSourceImpl(ServicePool.friendService))
+                HomeViewModel(repository) as T
+            }
+            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
+                val repository = UserRepositoryImpl(UserDataSourceImpl(ServicePool.userService))
+                MainViewModel(repository) as T
+            }
+            else -> throw IllegalArgumentException("Unknown ViewModel class")
         }
-        if (modelClass.isAssignableFrom(SignUpViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return authRepository?.let { SignUpViewModel(it) } as T
-        }
-        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return friendRepository?.let { HomeViewModel(it) } as T
-        }
-        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return userRepository?.let { MainViewModel(it) } as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
