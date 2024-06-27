26bc6afa4f770fc30655edd512a8b08d053b253a
yskim6772 <yskim6772@gmail.com>
2024-06-27 10:17:06 +0900

    Refactor: ApiService 반환값 수정

--- app/src/main/java/com/sopt/now/data/service/UserService.kt
+++ app/src/main/java/com/sopt/now/data/service/UserService.kt
@@ -1,13 +1,14 @@
 package com.sopt.now.data.service
 
+import com.sopt.now.data.dto.response.BaseResponse
+import com.sopt.now.data.dto.response.ResponseMemberInfo
 import com.sopt.now.data.dto.response.ResponseUserInfoDto
+import com.sopt.now.domain.entity.UserEntity
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
 
 interface UserService {
     @GET("member/info")
-    suspend fun getUserInfo(
-        @Header("memberId") userId: Int
-    ): ResponseUserInfoDto
+    suspend fun getUserInfo(): BaseResponse<ResponseMemberInfo>
 }
