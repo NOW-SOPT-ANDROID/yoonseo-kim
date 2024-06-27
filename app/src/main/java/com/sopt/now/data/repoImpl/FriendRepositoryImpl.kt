f63a8a658b6cd385643953427a12913740c0502d
yskim6772 <yskim6772@gmail.com>
2024-06-27 12:59:43 +0900

    Chore: 파라미터 Service를 DataSource로 수정

--- app/src/main/java/com/sopt/now/data/repoImpl/FriendRepositoryImpl.kt
+++ app/src/main/java/com/sopt/now/data/repoImpl/FriendRepositoryImpl.kt
@@ -1,13 +1,17 @@
 package com.sopt.now.data.repoImpl
 
+import com.sopt.now.data.ServicePool.friendService
+import com.sopt.now.data.datasource.FriendDataSource
 import com.sopt.now.domain.repository.FriendRepository
 import com.sopt.now.data.service.FriendService
 import com.sopt.now.domain.entity.FriendEntity
 
-class FriendRepositoryImpl(private val friendService: FriendService) : FriendRepository {
+class FriendRepositoryImpl(
+    private val friendDataSource: FriendDataSource
+) : FriendRepository {
 
     override suspend fun getFriends(page: Int): Result<List<FriendEntity>> = runCatching {
-        val response = friendService.getFriends(page)
+        val response = friendDataSource.getFriends(page)
         response.data
     }
 }
